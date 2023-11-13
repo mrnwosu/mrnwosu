2c30eacbde6854924511c03c19f53e5c3b30f9ef
Ike Nwosu <ike.nwosu.151@gmail.com>
2023-11-13 03:19:12 -0500

    An update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,19 @@
 import { type NextPage } from "next";
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 import Head from "next/head";
-import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
-
+import Image from "next/image";
 import { api } from "../utils/api";
 
 const Home: NextPage = () => {
-
-  const greetings = ['Hello!', '¡Bojour!', '¡Hola!', 'Hallo!', '¡Ola!']
-  const [greetingIndex, setIndex] = useState(0)
+  const greetings = ["Hello!", "¡Bojour!", "¡Hola!", "Hallo!", "¡Ola!"];
+  const [greetingIndex, setIndex] = useState(0);
 
   useEffect(() => {
     setTimeout(() => {
       setIndex((greetingIndex + 1) % 5);
-    }, 1000)
+    }, 1000);
   }, [greetingIndex]);
-
-  
-
 
   // const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
@@ -26,14 +21,38 @@ const Home: NextPage = () => {
     <>
       <Head>
         <title>Mr. Nwosu</title>
-        <meta name="description" content="Personal/Professional Website for Ike Nwosu" />
+        <meta
+          name="description"
+          content="Personal/Professional Website for Ike Nwosu"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex h-screen">
-        <div className=" bg-black opacity-30 relative w-screen h-5/6 overflow-hidden">
+      <main className="flex h-screen flex-row">
+        {/* <div className=" bg-black opacity-30 relative w-screen h-5/6 overflow-hidden">
           <video autoPlay muted loop src="./promo.mp4" className=" object-cover absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 -z-10"></video>
+        </div> */}
+        <div className=" w-1/3 bg-slate-500"></div>
+        <div className=" w-2/3 bg-slate-900">
+          <div className=" grid w-[27em] grid-cols-3 grid-rows-4 gap-2 ">
+            {Array(12)
+              .fill(0)
+              .map((_, i) => {
+                const number = i < 9 ? `00${i + 1}` : `0${i + 1}`;
+                return (
+                  <div key={i} className=" overflow-hidden">
+                    <Image
+                      src={`/brokenImage1/image_part_${number}.jpg`}
+                      alt="Author Image"
+                      width={140}
+                      height={157.89}
+                      sizes="100%"
+                      className="t"
+                    />
+                  </div>
+                );
+              })}
+          </div>
         </div>
-
       </main>
     </>
   );
@@ -46,11 +65,10 @@ const AuthShowcase: React.FC = () => {
 
   const { data: secretMessage } = api.example.getSecretMessage.useQuery(
     undefined, // no input
-    { enabled: sessionData?.user !== undefined },
+    { enabled: sessionData?.user !== undefined }
   );
 
   return (
-    <div className="flex flex-col items-center justify-center h-1/2">
-    </div>
+    <div className="flex h-1/2 flex-col items-center justify-center"></div>
   );
 };
