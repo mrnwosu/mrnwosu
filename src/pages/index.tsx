9f3b3598424d60bb5f6e8af3eaf1171e72c4ae8f
Ike Nwosu <ike.nwosu.151@gmail.com>
2023-11-18 11:03:22 -0500

    Update to text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -39,7 +39,7 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex h-screen w-screen flex-row bg-gradient-to-tr from-gray-600 to-claw_nueve">
         <div className="flex items-center justify-center sm:w-1/2 md:w-1/2 lg:w-1/2 ">
-          {/* <div className=" absolute left-12 top-12 h-96 w-96 animate-blob_move rounded-full bg-orange-400 blur-2xl opacity-40  mix-blend-multiply filter"></div> */}
+          <div className=" absolute left-12 top-12 h-96 w-96 animate-blob_move rounded-full bg-orange-400 blur-2xl opacity-40  mix-blend-multiply filter"></div>
           <div className=" absolute bottom-12 left-80 h-96 w-96 animate-blob_move rounded-full bg-claw_siete  opacity-50 mix-blend-multiply blur-2xl filter delay-1000"></div>
           <div className=" flex flex-col items-center justify-center gap-y-4 font-bold">
             <div className=" word-container text-7xl text-claw_siete opacity-0 transition delay-300 duration-[1500ms]">
@@ -53,7 +53,7 @@ const Home: NextPage = () => {
                 </p>
               </div>
               <div className=" word-container opacity-0 transition delay-700 duration-[1500ms]">
-                <p className="text-2xl text-claw_siete ">I does this.</p>
+                <p className="text-2xl text-claw_siete ">Software Engineer</p>
               </div>
               <div className="mt-8 flex flex-row gap-8 px-4">
                 {linkIcons.map((l, i) => {
