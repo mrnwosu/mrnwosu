44c3755093585f3c698f303c1c507e0de3d60c04
Ike Nwosu <ike.nwosu.151@gmail.com>
2023-12-23 19:29:37 -0500

    Updating layout for tabs/mobile

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -13,7 +13,7 @@ export const TopIconLink = function (props: LinkProps) {
   return (
     <Link
       href={props.href}
-      className={` flex flex-col items-center rounded-xl py-3 px-1 text-claw_diez transition duration-100 ease-in hover:text-claw_nueve`}
+      className={` flex flex-col items-center bg-claw_nueve/50 py-2 px-2 rounded-xl text-claw_diez transition duration-100 ease-in hover:text-claw_nueve`}
     >
       <props.icon sx={{ fontSize: props.size }} />
     </Link>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,6 +27,8 @@ const Home: NextPage = () => {
     ]);
   }, []);
 
+
+
   return (
     <>
       <Head>
@@ -37,25 +39,27 @@ const Home: NextPage = () => {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex h-screen w-screen flex-row bg-gradient-to-tr from-gray-600 to-claw_nueve">
-        <div className="flex items-center justify-center sm:w-1/2 md:w-1/2 lg:w-1/2 ">
-          <div className=" absolute left-12 top-12 h-96 w-96 animate-blob_move rounded-full bg-orange-400 blur-2xl opacity-40  mix-blend-multiply filter"></div>
-          <div className=" absolute bottom-12 left-80 h-96 w-96 animate-blob_move rounded-full bg-claw_siete  opacity-50 mix-blend-multiply blur-2xl filter delay-1000"></div>
+      <main className="relative flex flex-col md:flex-col lg:flex-row h-screen w-screen bg-gradient-to-tr from-gray-600 to-claw_nueve">
+        <div className=" absolute lg:relative z-10 flex flex-col justify-between items-center pt-72 pb-24 lg:justify-center h-screen w-full lg:w-1/2 ">
+          {/* <div className=" absolute left-12 top-12 h-96 w-96 animate-blob_move rounded-full bg-orange-400 blur-2xl opacity-40  mix-blend-multiply filter"></div>
+          <div className=" absolute bottom-12 left-80 h-96 w-96 animate-blob_move rounded-full bg-claw_siete  opacity-50 mix-blend-multiply blur-2xl filter delay-1000"></div> */}
           <div className=" flex flex-col items-center justify-center gap-y-4 font-bold">
             <div className=" word-container text-7xl text-claw_siete opacity-0 transition delay-300 duration-[1500ms]">
-              <p>Hello!</p>
+              <p className=" bg-slate-600/10 p-4 rounded-full">Hello!</p>
             </div>
             <div className="flex flex-col items-center justify-center gap-2">
-              <div className=" word-container tranistion text-right text-5xl text-claw_siete opacity-0 delay-500 duration-[1500ms]">
-                <p className="  ">
+              <div className=" flex flex-col items-center word-container tranistion text-right text-5xl text-claw_siete opacity-0 delay-500 duration-[1500ms]">
+                <p className=" bg-slate-600/10 p-4 rounded-full ">
                   My name is
                   <span className=" text-claw_dos"> Ike Nwosu</span>
                 </p>
               </div>
               <div className=" word-container opacity-0 transition delay-700 duration-[1500ms]">
-                <p className="text-2xl text-claw_siete ">Software Engineer</p>
+                <p className="text-2xl text-claw_siete p-2 rounded-full">Software Engineer</p>
               </div>
-              <div className="mt-8 flex flex-row gap-8 px-4">
+            </div>
+          </div>
+          <div className="mt-8 flex flex-row gap-8 px-4">
                 {linkIcons.map((l, i) => {
                   const delayClass = `delay-[${(i + 1) * 500}ms]`;
                   return (
@@ -74,11 +78,9 @@ const Home: NextPage = () => {
                   );
                 })}
               </div>
-            </div>
-          </div>
         </div>
-        <div className="flex w-1/2 items-center justify-center ">
-          <div className=" grid w-[27em] grid-cols-3 grid-rows-4 gap-2 ">
+        <div className=" flex items-center justify-center md:w-full lg:w-1/2 ">
+          <div className=" grid w-screen lg:w-1/2 p-12 lg:p-0 h-auto grid-cols-3 grid-rows-4 gap-2 ">
             {Array(12)
               .fill(0)
               .map((_, i) => {
@@ -89,13 +91,12 @@ const Home: NextPage = () => {
                 return (
                   <div
                     key={i}
-                    className={`${delay} profile-image-container relative opacity-0 shadow-md shadow-claw_diez transition duration-[1500ms] before:bg-black `}
+                    className={`${delay} profile-image-container aspect-[180/230] relative opacity-0 shadow-md shadow-claw_diez transition duration-[1500ms] before:bg-black `}
                   >
                     <Image
                       src={`/brokenImage1/image_part_${number}.jpg`}
                       alt="Author Image"
-                      width={140}
-                      height={157.89}
+                      layout="fill"
                       sizes="100%"
                       className="profile-image"
                       onLoad={() => {
